bb6f8843b6b05eb651852fb750ebfe58950e1bc1
akoroteev <akoroteev@justcoded.co>
2021-06-01 10:59:36 +0300

    first commit

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@vuepress/theme-default/layouts/Layout.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+  components: {
+    Layout,
+    Footer,
+  }
+}
+</script>
